0fee6160b88e34b9f3f9857cdb51dba3da074870
Frank Tan <tansongyang@gmail.com>
2017-07-28 22:59:32 -0400

    Add react-router.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="App">
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/profile" component={Profile}/>
+              <Route component={NoMatch}/>
+            </Switch>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
 
 export default App;
+
+function Home() {
+  return (
+    <div>Home</div>
+  );
+}
+
+function Profile() {
+  return (
+    <div>Profile</div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>404</div>
+  );
+}
+
