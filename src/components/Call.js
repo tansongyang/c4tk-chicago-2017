faf17ed48de231a8a3f3de3d32ae1efd62f8c530
Frank Tan <tansongyang@gmail.com>
2017-07-30 14:52:46 -0400

    Attempt to go back to reference code.
    We made minimal changes to get it to work with React.

--- src/components/Call.js
+++ src/components/Call.js
@@ -3,65 +3,72 @@ import { Redirect } from 'react-router-dom';
 import firebase from 'firebase';
 import './Call.css';
 
-const RTCPeerConnection = window.webkitRTCPeerConnection;
+//this makes sure that our code will work on different browsers
+var RTCPeerConnection = window.webkitRTCPeerConnection;
 
-const servers = {
-  'iceServers': [
-    {
-      urls: 'stun:stun.services.mozilla.com'
-    },
-    {
-      urls: 'stun:stun.l.google.com:19302'
-    },
-    {
-      urls: 'turn:numb.viagenie.ca',
-      credential: 'websitebeaver',
-      username: 'websitebeaver@email.com'
+//Create an account on Firebase, and use the credentials they give you in place of the following
+ var config = {
+   apiKey: "AIzaSyAqsquOVAUkNol9l7QttoTI1s3zXNA32Fw",
+   authDomain: "christianminglebutbetter.firebaseapp.com",
+   databaseURL: "https://christianminglebutbetter.firebaseio.com",
+   projectId: "christianminglebutbetter",
+   storageBucket: "christianminglebutbetter.appspot.com",
+   messagingSenderId: "904563004940"
+ };
+firebase.initializeApp(config);
+
+var database = () => firebase.database().ref();
+var yourVideo = () =>  document.getElementById("yourVideo");
+var friendsVideo = () => document.getElementById("friendsVideo");
+var yourId = null;
+var servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, {'urls': 'stun:stun.l.google.com:19302'}, {'urls': 'turn:numb.viagenie.ca','credential': 'websitebeaver','username': 'websitebeaver@email.com'}]};
+var pc = new RTCPeerConnection(servers);
+pc.onicecandidate = (event => event.candidate?sendMessage(yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
+pc.onaddstream = (event => friendsVideo().srcObject = event.stream);
+
+function sendMessage(senderId, data) {
+    var msg = database().push({ sender: senderId, message: data });
+    msg.remove();
+}
+
+function readMessage(data) {
+    if (!data.val().message) {
+      return;
     }
-  ]
+    var msg = JSON.parse(data.val().message);
+    var sender = data.val().sender;
+    if (sender != yourId) {
+        if (msg.ice != undefined)
+            pc.addIceCandidate(new RTCIceCandidate(msg.ice));
+        else if (msg.sdp.type == "offer")
+            pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
+              .then(() => pc.createAnswer())
+              .then(answer => pc.setLocalDescription(answer))
+              .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})));
+        else if (msg.sdp.type == "answer")
+            pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+    }
 };
-const pc = new RTCPeerConnection(servers);
 
-function sendMessage(sender, target, data) {
-  const msg = firebase.database().ref().push({
-    sender,
-    target,
-    message: data
-  });
-  msg.remove();
+database().on('child_added', readMessage);
+
+function showMyFace() {
+  navigator.mediaDevices.getUserMedia({audio:true, video:true})
+    .then(stream => yourVideo().srcObject = stream)
+    .then(stream => pc.addStream(stream));
 }
 
-function readMessage(sender, target, data) {
-  const val = data.val();
-  if (!val.message) {
-    return;
-  }
-
-  const msg = JSON.parse(val.message);
-  if (val.sender === sender) {
-    return;
-  }
-
-  if (msg.ice) {
-    pc.addIceCandidate(new RTCIceCandidate(msg.ice));
-  } else if (msg.sdp.type === 'offer') {
-    pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
-      .then(() => pc.createAnswer())
-      .then((answer) => pc.setLocalDescription(answer))
-      .then(() => {
-        sendMessage(
-          sender,
-          target,
-          JSON.stringify({
-            sdp: pc.localDescription
-          }));
-      });
-  } else if (msg.sdp.type == "answer") {
-    pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
-  }
+function showFriendsFace() {
+  pc.createOffer()
+    .then(offer => pc.setLocalDescription(offer) )
+    .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})) );
 }
-
 export default class Call extends React.Component {
+  constructor(props) {
+    super(props);
+    yourId = firebase.auth().currentUser.uid;
+  }
+
   getSelectedId() {
     return this.props.match.params.id;
   }
@@ -70,51 +77,51 @@ export default class Call extends React.Component {
     return this.props.match.params.target;
   }
 
-  showFriend() {
-    pc.createOffer()
-      .then((offer) => pc.setLocalDescription(offer))
-      .then(() => {
-        sendMessage(
-          this.getSelectedId(),
-          this.getTargetId(),
-          JSON.stringify({
-            sdp: pc.localDescription
-          }));
-      });
-  }
+  // showFriend() {
+  //   pc.createOffer()
+  //     .then((offer) => pc.setLocalDescription(offer))
+  //     .then(() => {
+  //       sendMessage(
+  //         this.getSelectedId(),
+  //         this.getTargetId(),
+  //         JSON.stringify({
+  //           sdp: pc.localDescription
+  //         }));
+  //     });
+  // }
 
-  showSelf() {
-    navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then((stream) => {
-      if (this.ourVideo) {
-        this.ourVideo.srcObject = stream;
-        pc.addStream(stream);
-      }
-    });
-  }
+  // showSelf() {
+  //   navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then((stream) => {
+  //     if (this.ourVideo) {
+  //       this.ourVideo.srcObject = stream;
+  //       pc.addStream(stream);
+  //     }
+  //   });
+  // }
 
   componentDidMount() {
-    pc.onicecandidate = (event) => {
-      if (event.candidate) {
-        sendMessage(
-          this.getSelectedId(),
-          this.getTargetId(),
-          JSON.stringify({
-            ice: event.candidate
-          }));
-      } else {
-        console.log('Send All Ice');
-      }
-    };
-    pc.onaddstream = (event) => {
-      if (this.theirVideo) {
-        this.theirVideo.srcObject = event.stream;
-      }
-    };
-    firebase.database().ref().on('child_added', (data) => readMessage(
-      this.getSelectedId(),
-      this.getTargetId(),
-      data));
-    this.showSelf();
+    // pc.onicecandidate = (event) => {
+    //   if (event.candidate) {
+    //     sendMessage(
+    //       this.getSelectedId(),
+    //       this.getTargetId(),
+    //       JSON.stringify({
+    //         ice: event.candidate
+    //       }));
+    //   } else {
+    //     console.log('Send All Ice');
+    //   }
+    // };
+    // pc.onaddstream = (event) => {
+    //   if (this.theirVideo) {
+    //     this.theirVideo.srcObject = event.stream;
+    //   }
+    // };
+    // firebase.database().ref().on('child_added', (data) => readMessage(
+    //   this.getSelectedId(),
+    //   this.getTargetId(),
+    //   data));
+    showMyFace();
   }
 
   render() {
@@ -128,19 +135,21 @@ export default class Call extends React.Component {
     return (
       <div className="Call">
         <video
+          id="yourVideo"
           autoPlay
           className="Call-video Call-ourVideo"
           muted
           ref={(video) => this.ourVideo = video}
         />
         <video
+          id="friendsVideo"
           autoPlay
           className="Call-video Call-theirVideo"
           ref={(video) => this.theirVideo = video}
         />
         <button
-            className="Call-button"
-          onClick={() => this.showFriend()}
+          className="Call-button"
+          onClick={() => showFriendsFace()}
         >
           Retry
         </button>
