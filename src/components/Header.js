41eb321e241c508f620abd0d96e457d5cc64d51f
Frank Tan <tansongyang@gmail.com>
2017-07-30 11:08:19 -0400

    Add logout button.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,23 +4,30 @@ import firebase from 'firebase';
 import './Header.css';
 
 class Home extends Component {
+  login() {
+    this.props.history.push({
+      pathname: '/login'
+    });
+  }
+
   logout() {
     firebase.auth().signOut().then(() => {
-        this.props.history.push({
-          pathname: '/login'
-        })
+      this.login();
     });
   }
 
   render() {
+    const isLogin = this.props.location.pathname === '/login';
     return (
       <header className="Header">
         <span className="Header-title">notanisland</span>
-        {firebase.auth().currentUser && (
+        {firebase.auth().currentUser ? (
           <span>
             <span className="Header-welcome">Welcome back, {firebase.auth().currentUser.email}!</span>
-            <button classname="Header-logout" onClick={() => this.logout()}>Logout</button>
+            <button classname="Header-logout" onClick={() => this.logout()}>Log out</button>
           </span>
+        ) : !isLogin && (
+          <button classname="Header-login" onClick={() => this.login()}>Log in</button>
         )}
       </header>
     );
